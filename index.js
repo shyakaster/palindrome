70602fe0f228907cfbbe3ede9eb2c2cb926f36a1
alex shyaka <shyakaster@gmail.com>
2018-09-24 12:29:15 +0300

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -10,27 +10,16 @@ String.prototype.reverse = function reverseFunction(){
 function Phrase(content){
     this.content = content;
     this.processContent = function processContent(){
-        return this.content.toLowerCase();
+        return this.letters().toLowerCase();
     }
     this.palindrome = function palindrome(){
         return this.processContent() === this.processContent().reverse();
     }
+    //Return the content without punctuation
+    this.letters = function letters(){
+        //this is beautiful peace of functional programming
+        return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+    }
 }
-
-//Defines a translatedPhrase object
-// function translatedPhrase(content, translation){
-//     this.content = content;
-//     this.translation = translation;
-
-//     this.processContent = function processContent(){
-//         return this.translation.toLowerCase();
-//     }
-// } 
-
-// translatedPhrase.prototype = new Phrase();
-
-// String.prototype.palindrome = function palindrome(){
-//     return new Phrase(this).palindrome();
-// }
 
 
